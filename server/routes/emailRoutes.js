adbac952c402bf452507c59f99f1cc6ba4529b6d
rita.radovics <krumplisbalhe@gmail.com>
2019-11-19 21:00:31 +0100

    Trying to set up gitignore

--- server/routes/emailRoutes.js
+++ server/routes/emailRoutes.js
@@ -24,7 +24,35 @@ router.post(
       const user = users[0]
 
 			if (req.body.to && req.body.text && req.body.username) {
-					
+				async function main() {
+					// create reusable transporter object using the default SMTP transport
+
+					let transporter = nodemailer.createTransport({
+						service: 'gmail',
+						auth: {
+									 user: 'youremail@address.com',
+									 pass: 'yourpassword'
+							 }
+					 });
+
+					// send mail with defined transport object
+					let info = await transporter.sendMail({
+							from: '"Fred Foo 👻" <foo@example.com>', // sender address
+							to: 'bar@example.com, baz@example.com', // list of receivers
+							subject: 'Hello ✔', // Subject line
+							text: 'Hello world?', // plain text body
+							html: '<b>Hello world?</b>' // html body
+					});
+
+					console.log('Message sent: %s', info.messageId);
+					// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+					// Preview only available when sending through an Ethereal account
+					console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+					// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+				}
+
+				main().catch(console.error);
 			} else {
 					console.log(error)
 					res.status(400).json({response: "Required user data for accessing this page is missing." })
