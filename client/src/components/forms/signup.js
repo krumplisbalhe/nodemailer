75bac7996b381d1977384d9b2cb4c6d5f12a6e1d
rita.radovics <krumplisbalhe@gmail.com>
2019-11-20 16:47:08 +0100

    sign up works

--- client/src/components/forms/signup.js
+++ client/src/components/forms/signup.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Signup() {
+  const [signupData, setSignupData] = useState({username: '', email: '', password: ''})
+  const handleSignupDataChange = (key, value) => {
+    setSignupData({...signupData, [key]: value});
+  }
+
+  const submit = event => {
+    event.preventDefault()
+    fetch("http://localhost:8080/users/signup", {
+      method: 'POST',
+      body: JSON.stringify(signupData),
+      headers: {
+        'Content-Type': 'application/json'
+    }
+    })
+    .then(res => res.json())
+    .then(response => {
+      console.log(response)
+    })
+  }
+
   return (
     <div className="signup">
-      signup function
+      <form onSubmit={submit}>
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" onChange={event => handleSignupDataChange('username', event.target.value)}></input>
+        <label htmlFor="email">Email address</label>
+        <input type="email" name="email" onChange={event => handleSignupDataChange('email', event.target.value)}></input>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" onChange={event => handleSignupDataChange('password', event.target.value)}></input>
+        <input type="submit" value="Log-in"></input>
+      </form>
     </div>
   )
 }
